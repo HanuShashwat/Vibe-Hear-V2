b420b8d59e1731c414c11217daa5d403633a9461
Hanu Shashwat <hanushashwat733@gmail.com>
2026-04-08 00:20:02 +0530

    feat(speech_state.dart): add transcriptHistory and currentTranscript properties to store speech recognition data

--- lib/blocs/speech/speech_state.dart
+++ lib/blocs/speech/speech_state.dart
@@ -4,25 +4,33 @@ class SpeechState extends Equatable {
   final bool isListening;
   final bool isMicAvailable;
   final String detectionMessage;
+  final List<String> transcriptHistory;
+  final String currentTranscript;
 
   const SpeechState({
     this.isListening = false,
     this.isMicAvailable = false,
     this.detectionMessage = 'Initializing...',
+    this.transcriptHistory = const [],
+    this.currentTranscript = '',
   });
 
   SpeechState copyWith({
     bool? isListening,
     bool? isMicAvailable,
     String? detectionMessage,
+    List<String>? transcriptHistory,
+    String? currentTranscript,
   }) {
     return SpeechState(
       isListening: isListening ?? this.isListening,
       isMicAvailable: isMicAvailable ?? this.isMicAvailable,
       detectionMessage: detectionMessage ?? this.detectionMessage,
+      transcriptHistory: transcriptHistory ?? this.transcriptHistory,
+      currentTranscript: currentTranscript ?? this.currentTranscript,
     );
   }
 
   @override
-  List<Object> get props => [isListening, isMicAvailable, detectionMessage];
+  List<Object> get props => [isListening, isMicAvailable, detectionMessage, transcriptHistory, currentTranscript];
 }
