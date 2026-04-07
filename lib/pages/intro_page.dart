c451a1a9191d81955392e43f708a02bbadee58fa
Hanu Shashwat <hanushashwat733@gmail.com>
2026-04-07 23:22:30 +0530

    refactor(IntroPage): convert stateful widget to stateless widget

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:vibehear/pages/setup_page.dart';
 
-class IntroPage extends StatefulWidget {
+class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
-  @override
-  State<IntroPage> createState() => _IntroPageState();
-}
-
-class _IntroPageState extends State<IntroPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
