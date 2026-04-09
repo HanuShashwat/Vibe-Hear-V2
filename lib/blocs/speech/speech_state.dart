680f02ca74a8a7a8a17b2e47012b6126f169ba43
Hanu Shashwat <hanushashwat733@gmail.com>
2026-04-09 23:38:48 +0530

    feat(speech_state.dart): add isPaused property to track speech pause state

--- lib/blocs/speech/speech_state.dart
+++ lib/blocs/speech/speech_state.dart
@@ -3,6 +3,7 @@ import 'package:equatable/equatable.dart';
 class SpeechState extends Equatable {
   final bool isListening;
   final bool isMicAvailable;
+  final bool isPaused;
   final String detectionMessage;
   final List<String> transcriptHistory;
   final String currentTranscript;
@@ -10,6 +11,7 @@ class SpeechState extends Equatable {
   const SpeechState({
     this.isListening = false,
     this.isMicAvailable = false,
+    this.isPaused = false,
     this.detectionMessage = 'Initializing...',
     this.transcriptHistory = const [],
     this.currentTranscript = '',
@@ -18,6 +20,7 @@ class SpeechState extends Equatable {
   SpeechState copyWith({
     bool? isListening,
     bool? isMicAvailable,
+    bool? isPaused,
     String? detectionMessage,
     List<String>? transcriptHistory,
     String? currentTranscript,
@@ -25,6 +28,7 @@ class SpeechState extends Equatable {
     return SpeechState(
       isListening: isListening ?? this.isListening,
       isMicAvailable: isMicAvailable ?? this.isMicAvailable,
+      isPaused: isPaused ?? this.isPaused,
       detectionMessage: detectionMessage ?? this.detectionMessage,
       transcriptHistory: transcriptHistory ?? this.transcriptHistory,
       currentTranscript: currentTranscript ?? this.currentTranscript,
@@ -32,5 +36,5 @@ class SpeechState extends Equatable {
   }
 
   @override
-  List<Object> get props => [isListening, isMicAvailable, detectionMessage, transcriptHistory, currentTranscript];
+  List<Object> get props => [isListening, isMicAvailable, isPaused, detectionMessage, transcriptHistory, currentTranscript];
 }
